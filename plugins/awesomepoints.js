4bf1f6b22dc862bfd32affef583864da6f678855
Yu Chen Hou <houyuchen66@gmail.com>
2015-02-11 12:41:05 -0500

    Awesome has never been quantified like this before.

---
+++ plugins/awesomepoints.js
@@ -0,0 +1,31 @@
+module.exports = function(pluto) {
+    require("shelljs/global");
+    require('es6-promise').polyfill();
+    var request = require('popsicle');
+    var welcomeModule = {};
+
+    pluto.get("/score", function(req, res) {
+        res.send("Everyone's a winner (unimplemented feature)");
+    });
+
+    welcomeModule.listeners = {
+        "users::signin": function(user) {
+            if (user.name == "Andrew") {
+                exec('echo "Hello, ' + user.name + '. Once you start Russian, there\'s no time for Stalin." | festival --tts');
+            } else {
+                request("http://www.davepagurek.com/badjokes/joke").then(function(res) {
+                    joke = JSON.parse(res.body);
+                    exec('echo "Hello, ' + user.name + '. ' + joke.q + ' ' + joke.a + '" | festival --tts');
+                });
+            }
+        },
+        "users::signout": function(user) {
+            exec('echo "Goodbye, ' + user.name + '" | festival --tts');
+        },
+        "users::register": function(user) {
+            exec('echo "Hello, ' + user.name + '. Nice to meet you." | festival --tts');
+        }
+    };
+
+    return welcomeModule;
+}
