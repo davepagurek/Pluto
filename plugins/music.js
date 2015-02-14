454f98181102d2c84612e2e4cbae4c2bee93e62c
Dave Pagurek <dave@pahgawks.com>
2015-02-14 13:03:03 -0500

    Selecting random song works

--- plugins/music.js
+++ plugins/music.js
@@ -1,4 +1,7 @@
 module.exports = function(pluto) {
+    require('es6-promise').polyfill();
+    var request = require('popsicle');
+
     var data = pluto.getStorage("users")||{};
 
     var musicModule = {};
@@ -11,14 +14,30 @@ module.exports = function(pluto) {
             if (data[user].in && data[user].artists) {
                 data[user].artists.forEach(function(artist) {
                     //Add artist if it doesn't already exist
-                    console.log("checking artist " + artist);
                     if (artists.indexOf(artist) == -1) artists.push(artist);
                 });
             }
         }
-        console.log(artists);
         var selectedArtist = artists[Math.floor(Math.random()*artists.length)];
-        console.log("Selected artist: " + selectedArtist);
+        var artist = "";
+        console.log("Requesting https://api.spotify.com/v1/search?q=" + encodeURIComponent(selectedArtist) + "&type=artist");
+        request("https://api.spotify.com/v1/search?q=" + encodeURIComponent(selectedArtist) + "&type=artist").then(function(res) {
+            if (res.status != 200) {
+                console.log("An error occurred: response " + res.status);
+                return;
+            }
+            artist = res.body.artists.items[0];
+            console.log("Requesting https://api.spotify.com/v1/artists/" + artist.id + "/top-tracks?country=US");
+            request("https://api.spotify.com/v1/artists/" + artist.id + "/top-tracks?country=US").then(function(res) {
+                if (res.status != 200) {
+                    console.log("An error occurred: response " + res.status);
+                    return;
+                }
+                songs = res.body.tracks;
+                var selectedSong = songs[Math.floor(Math.random()*songs.length)];
+                console.log("Selected song: " + selectedSong.name + " - " + selectedArtist);
+            });
+        });
 
     };
 
