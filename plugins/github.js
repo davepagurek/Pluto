03cb58634f631c183558f5a8f55468c424b10dbd
Dave Pagurek <dave@pahgawks.com>
2015-02-27 18:29:37 -0500

    Fixed issue checking github when users register

--- plugins/github.js
+++ plugins/github.js
@@ -10,13 +10,15 @@ module.exports = function(pluto) {
     var lastCheck = pluto.getStorage("github")||0;
 
     var checkGithub = function(user) {
-        console.log("Requesting " + "https://github.com/" + data[user].github);
-        request("https://github.com/" + data[user].github).then(function(res) {
-            var match = /<span class="text-muted">Current streak<\/span>\s+<span class="contrib-number">([0-9]+) day(?:s?)<\/span>/.exec(res.body);
-            if (match) {
-                pluto.emitEvent("points::awardTo", data[user], parseInt(match[1]));
-            }
-        });
+        if (data[user].github) {
+            console.log("Requesting " + "https://github.com/" + data[user].github);
+            request("https://github.com/" + data[user].github).then(function(res) {
+                var match = /<span class="text-muted">Current streak<\/span>\s+<span class="contrib-number">([0-9]+) day(?:s?)<\/span>/.exec(res.body);
+                if (match) {
+                    pluto.emitEvent("points::awardTo", data[user], parseInt(match[1]));
+                }
+            });
+        }
     };
 
     //Run this every four hours to make sure it doesn't wait too long
@@ -35,7 +37,7 @@ module.exports = function(pluto) {
 
     githubModule.listeners = {
         "users::register": function(user) {
-            checkGithub(user);
+            checkGithub(user.ip);
         }
     };
 
