811eaaed71a65ce04e0d0d4c73da38b83dd78d54
Andrew Codispoti <andrewcod749@gmail.com>
2015-08-11 22:03:59 -0400

    renamed oops.

--- plugins/muzikdriver.js
+++ plugins/muzikdriver.js
@@ -1,11 +1,12 @@
 var request = require('request');
 module.exports = {
-  getLinks : function (song,callback){
+  getLink : function (song,callback){
     request('http://muzik.elasticbeanstalk.com/search?songname='+song.name,function(error,response,body){
       var bodyObj = JSON.parse(body);
       var urls = bodyObj.url;
       var firstResult = urls[0];
       var url = firstResult[Object.keys(firstResult)[0]];
+      console.log(url);
       callback(url);
     });
   }
