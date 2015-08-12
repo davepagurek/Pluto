7562f0e7af218e57c958132c49cec20243d4f1fd
Andrew Codispoti <andrewcod749@gmail.com>
2015-08-11 21:52:56 -0400

    Function is called getLinks.

--- plugins/muzikdriver.js
+++ plugins/muzikdriver.js
@@ -1,11 +1,12 @@
 var request = require('request');
-module.exports = function (song,callback){
-  request('http://muzik.elasticbeanstalk.com/search?songname='+song.name,function(error,response,body){
-    var bodyObj = JSON.parse(body);
-    var urls = bodyObj.url;
-    var firstResult = urls[0];
-    var url = firstResult[Object.keys(firstResult)[0]];
-    callback(url);
-  });
-};
-
+module.exports = {
+  getLinks : function (song,callback){
+    request('http://muzik.elasticbeanstalk.com/search?songname='+song.name,function(error,response,body){
+      var bodyObj = JSON.parse(body);
+      var urls = bodyObj.url;
+      var firstResult = urls[0];
+      var url = firstResult[Object.keys(firstResult)[0]];
+      callback(url);
+    });
+  }
+}
