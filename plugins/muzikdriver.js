2a8c5451ed0799653e1ff821c6f5bf00dafdebfe
Dave Pagurek <dave@davepagurek.com>
2015-08-19 21:44:34 -0400

    Get most relevant Muzik result

--- plugins/muzikdriver.js
+++ plugins/muzikdriver.js
@@ -1,13 +1,18 @@
 var request = require('popsicle');
+var Levenshtein = require("levenshtein");
 module.exports = {
-  getLink : function (song,callback){
-    request('http://muzik.elasticbeanstalk.com/search?songname='+ song.name.shellEscape() + " " + song.artist.shellEscape())
-    .then(function(res){
-      var body = JSON.parse(res.body);
-      var urls = body.url;
-      var firstResult = urls[0];
-      var url = firstResult[Object.keys(firstResult)[0]];
-      callback(Object.keys(firstResult)[0],url);
-    });
-  }
+    getLink: function (song,callback){
+        request('http://muzik.elasticbeanstalk.com/search?songname='+ song.name + " " + song.artist)
+        .then(function(res){
+            var body = JSON.parse(res.body);
+            var urls = body.url;
+            var result = _.min(_.filter(urls, function(element) {
+                return !!element[Object.keys(element)[0]].match(/\.mp3$/);
+            }), function(element) {
+                return new Levenshtein(song.name, Object.keys(element)[0]).distance;
+            });
+            var url = result[Object.keys(result)[0]];
+            callback(Object.keys(result)[0],url);
+        });
+    }
 }
