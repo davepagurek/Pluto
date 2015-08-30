1101c584106b106744ebbecd429bc342ed5fd544
Andrew Codispoti <andrewcod749@gmail.com>
2015-08-30 15:25:29 -0400

    Dont render page anymore. Just redirect to trello board so that when on mobile you can deep link to the trello application.

--- plugins/trellohandler.js
+++ plugins/trellohandler.js
@@ -1,9 +1,7 @@
 module.exports = function(pluto){
   var trelloModule = {};
   pluto.get("/board",function(req,res){
-    res.render("trello.html",{
-        "layout":false
-    });
+    res.redirect("https://trello.com/b/VgNPAYxd");
   });
   return trelloModule;
 }
