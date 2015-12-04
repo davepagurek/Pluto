c973bb0a1e7d48f22aae384b331fd15c4b503a07
Kong-Artist <stevendjkong@gmail.com>
2015-12-04 16:22:01 -0500

    Added some styling to playlist adding + readded config example

--- public/javascripts/music_frontend.js
+++ public/javascripts/music_frontend.js
@@ -41,3 +41,39 @@ if (songProgressContainer) {
     updateProgressFromServer();
 }
 
+(function makeTabs() {
+
+    tab_lists_anchors = document.querySelectorAll(".adding_tabs li a");
+    divs = document.querySelectorAll("#tabs > div");
+    for (var i = 0; i < tab_lists_anchors.length; i++) {
+        if (tab_lists_anchors[i].classList.contains('active')) {
+            divs[i].style.flex = "1";
+        }
+
+    }
+
+    for (i = 0; i < tab_lists_anchors.length; i++) {
+
+        document.querySelectorAll(".adding_tabs li a")[i].addEventListener('click', function(e) {
+
+            for (i = 0; i < divs.length; i++) {
+                divs[i].style.flex = null;
+            }
+
+            for (i = 0; i < tab_lists_anchors.length; i++) {
+                tab_lists_anchors[i].classList.remove("active");
+            }
+
+            clicked_tab = e.target || e.srcElement;
+
+            clicked_tab.classList.add('active');
+            div_to_show = clicked_tab.getAttribute('href');
+            console.log(document.querySelector(div_to_show));
+            document.querySelector(div_to_show).style.flex = "1";
+
+            e.preventDefault();
+        });
+    }
+
+})();
+
